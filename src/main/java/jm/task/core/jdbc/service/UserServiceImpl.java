aba527bd0f0d85ce9334831b6b53c3870937741c
shatijr <ranzolaa@gmail.com>
2024-10-14 22:53:34 +0500

    commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -3,41 +3,35 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoJDBCImpl();
+
     @Override
     public void createUsersTable() {
-        userDao = new UserDaoJDBCImpl();
         userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userDao = new UserDaoJDBCImpl();
         userDao.dropUsersTable();
-
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDao = new UserDaoJDBCImpl();
         userDao.saveUser(name, lastName, age);
 
     }
 
     public void removeUserById(long id) {
-        userDao = new UserDaoJDBCImpl();
         userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-
-        userDao = new UserDaoJDBCImpl();
         return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userDao = new UserDaoJDBCImpl();
         userDao.cleanUsersTable();
     }
 }
